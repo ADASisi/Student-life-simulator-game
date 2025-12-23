552eac16632dee3f374f5e0c2cf30f7164792fb9
santova <100036255+ADASisi@users.noreply.github.com>
2025-12-23 12:13:12 +0200

    add the other actions
    rest, work, going out, eating
    fix structures and efficiency

--- students_life/students_life/students_life.cpp
+++ students_life/students_life/students_life.cpp
@@ -5,18 +5,19 @@ const int BEGINNIG_VALUE = 100;
 
 struct Person
 {
-    int knowledge = BEGINNIG_VALUE;
-    int mentality = BEGINNIG_VALUE;
-    int energy = BEGINNIG_VALUE;
-    int money = BEGINNIG_VALUE;
+    int knowledge;
+    int mentality;
+    int energy;
+    int money;
     int examsPassed = 0;
 };
 
-struct Game
+struct GameState
 {
     const int totalDays = 45;
     int currentDay = 1;
-
+	Person player;
+    int examsDays[5];
 };
 
 float getEfficiency(int enregy)
@@ -40,20 +41,56 @@ void studyingAtHome(Person* p)
 {
     float efficiency = getEfficiency(p->energy);
     p->knowledge += 15 * efficiency;
-    p->energy -= 15;
-    p->mentality -= 20;
+    p->energy -= 15 * efficiency;
+    p->mentality -= 20 * efficiency;
 }
 
 void studyingWithFriends(Person* p)
 {
     float efficiency = getEfficiency(p->energy);
     p->knowledge += 5 * efficiency;
-    p->energy -= 10;
+    p->energy -= 10 * efficiency;
+    p->mentality += 10 * efficiency;
+}
+
+void eating(Person* p)
+{
+    float efficiency = getEfficiency(p->energy);
+    p->mentality += 5 * efficiency;
+    p->energy += 20 * efficiency;
+    p->money -= 10 * efficiency;
+}
+
+void goingOut(Person* p)
+{
+    float efficiency = getEfficiency(p->energy);
+    p->mentality += 40 * efficiency;
+    p->energy -= 15 * efficiency;
+    p->money -= 25 * efficiency;
+}
+
+void sleeping(Person* p)
+{
+    p->energy += 50;
     p->mentality += 10;
+}
+
+void shiftWork(Person* p)
+{
+    float efficiency = getEfficiency(p->energy);
+    p->mentality -= 10 * efficiency;
+    p->energy -= 20 * efficiency;
+    p->money += 40 * efficiency;
+}
+
+void takingExam(Person* p)
+{
+    p->energy -= 20;
 }
 
 int main()
 {
+
     Person *p;
     return 0;
 }
